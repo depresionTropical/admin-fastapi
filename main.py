01c327c13e7b8483b5bceb2e0ea82d6991da367f
Hugo Martinez <le19080710@itmerida.edu.mx>
2024-04-02 22:14:27 -0600

    update new project

--- main.py
+++ main.py
@@ -1,38 +1,13 @@
 from fastapi import FastAPI
-from fastapi.params import Body
-from uuid import uuid1
 
-# import models
-from models.post import Post
 
-# import data
-from data.my_data import posts as my_posts
-
-# create an instance of FastAPI
 app = FastAPI()
 
-# create a route
-@app.get("/")
-async def root():
-  return {"message": "Hello World"}
+@app.get('/', tags=['Home'])
+def home():
+    return {'message': 'Welcome to the API'}
 
-@app.get("/posts")
-def get_post():
-  return {"data": my_posts}
+@app.get('/movies', tags=['Home'])
+def home():
+    return {'message': 'Welcome to the Home page'}
 
-@app.get("/posts/{post_id}")
-def get_post_by_id(post_id: int):
-  # return {"id": post_id}
-  post = find_post(post_id)
-  return {"data": post}
-
-def find_post(post_id: int):
-  return next((post for post in my_posts if post["id"] == post_id), None)
-  
-
-@app.post("/posts")
-def create_post(post : Post):
-  post_dict = post.dict()
-  post_dict["id"] = uuid1().int
-  my_posts.append(post_dict)
-  return {"message": "Post is created successfully"}
